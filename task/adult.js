27d745195b4622f70f23628f83e4883fdb2dbe39
Roman <rmorozovas@gmail.com>
2025-03-25 09:26:06 +0200

    update

---
+++ task/adult.js
@@ -0,0 +1,19 @@
+/* 
+Reikalinga funkcija, kuri gauna vartotojo amziu (metai) ir atitinkamai grazina zodi :
+- suauges
+- vaikas
+Amziaus ribos kriterijus : >= 18 metu
+*/ 
+
+function isAdult(age) {
+if (age >= 18) {
+    return 'suauges'
+} else {
+    return 'vaikas';
+}
+    }
+    
+console.log(isAdult(99), '-->', 'suauges');
+console.log(isAdult(18), '-->', 'suauges');
+console.log(isAdult(17), '-->', 'vaikas');
+console.log(isAdult(1), '-->', 'vaikas');
