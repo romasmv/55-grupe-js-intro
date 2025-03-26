dbcd302577fe343c86913ce823d52e63a20398e4
Roman <rmorozovas@gmail.com>
2025-03-26 16:15:19 +0200

    update

--- homework/gabijos_uzduotis.js
+++ homework/gabijos_uzduotis.js
@@ -95,17 +95,88 @@ console.clear();
 // 1. Patikrinkite, ar sąraše yra tam tikras skaičius.
      // a. Pavyzdys: Patikrinkite, ar sąraše [1, 2, 3, 4] yra skaičius 3.
 
-const sarasas1 = [1, 2, 3, 4];
+let sarasas1 = [1, 2, 3, 4];
 function tikrintiSarasa(sarasas1){
 if (sarasas1.includes(3) ) {
     return 'Taip yra 3'}
  else {return 'Nera'};
 }
-
 console.log(tikrintiSarasa(sarasas1))
+
+
 
 // 2. Patikrinkite, ar masyvas tuščias.
      // a. Pavyzdys: Patikrinkite, ar sąrašas [] yra tuščias.
    
- 
+ let sarasas2 = [];
+ if (sarasas2.length === 0) {
+    console.log('Taip masyvas tuščias');}
+    else { ('Masyvas Nera tuščias')
+    };
+
+//   3. Nustatyti, ar sąraše yra teigiamas skaičius.
+     // a. Pavyzdys: Patikrinkite, ar sąraše [1, -2, 3, -4] yra teigiamų skaičių.
+
+let sarasas3 = [1, -2, 3, -4];
+
+if (sarasas3[0] > 0 || sarasas3[1] > 0 || sarasas3[2] > 0 || sarasas3[3] > 0) {
+    console.log('Taip sąraše yra teigiamas skaičius'); 
+} else { console.log('NE sąraše nera teigiamas skaičius');
+
+}
+
+// 4. Patikrinkite, ar visi sąrašo skaičiai yra lyginiai.
+     // a. Pavyzdys: Patikrinkite, ar [2, 4, 6] yra tik lyginiai skaičiai.
+
+     let sarasas4 = [2, 4, 6];
+
+     if (sarasas4[0] % 2===0 && sarasas4[1] % 2 ===0 && sarasas4[2] % 2 ===0 ) {
+         console.log('Taip visi sąrašo skaičiai yra lyginiai'); 
+     } else { console.log('NE visi sąrašo skaičiai nera lyginiai');}
+
+//  5. Patikrinkite, ar kuris nors skaičius sąraše yra neigiamas.
+     // a. Pavyzdys: Patikrinkite, ar kuris nors skaičius sąraše [3, 5, -2] yra neigiamas.   
+
+     let sarasas5 = [3, 5, -2];
+
+     if (sarasas5[0] < 0 || sarasas5[1] < 0 || sarasas5[2] < 0 ) {
+         console.log('Taip sąraše [3, 5, -2] yra neigiamas skaicius'); 
+     } else { console.log('NE sąraše [3, 5, -2] nera neigiamo skaiciaus');}
+
+//    6. Patikrinkite, ar pirmas skaičius sąraše yra didesnis už paskutinį.
+     // a. Pavyzdys: Patikrinkite, ar pirmas skaičius sąraše [1, 3, 5] yra didesnis už paskutinį.
+
+     let sarasas6 = [1, 3, 5];
+
+     if (sarasas6[0] > sarasas6[2]) {
+         console.log('Pirmas skaičius sąraše yra didesnis už paskutinį'); 
+     } else { console.log('NE pirmas skaičius sąraše nera didesnis už paskutinį');}
+
+//    7. Patikrinkite, ar sąrašo ilgis didesnis už 5.
+     // a. Pavyzdys: Patikrinkite, ar sąrašo [1, 2, 3, 4, 5, 6] ilgis yra didesnis nei 5.
+
+     let sarasas7 = [1, 2, 3, 4, 5, 7];
+
+     if (sarasas7.length > 5 ) {
+         console.log('Taip sąrašo ilgis yra didesnis nei 5'); 
+     } else { console.log('NE sąrašo ilgis yra mažesnis nei 5');}
+
+//   8. Patikrinkite, ar sąraše yra tik teksto tipo kintamieji
+    //a. Pavyzdys: Patikrinkite, ar [„obuolys“, ‚bananas‘, „vyšnia“] yra tik tekstai.
+
+    let sarasas8 = ["obuolys", "bananas", "vyšnia"];
+
+    if (sarasas8[0] === "String" && sarasas8[1] ==="String" && sarasas8[2] ==="String") {
+        console.log('Taip sąrašo ilgis yra didesnis nei 5'); 
+    } else { console.log('NE sąrašo ilgis yra mažesnis nei 5');}
+
+
+
+
+
+
+
+
+
+
 
