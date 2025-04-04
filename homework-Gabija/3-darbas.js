28568f9e020031dbbf92f28cc7a084f65b762dae
Roman <rmorozovas@gmail.com>
2025-04-04 20:00:00 +0300

    update 3-darbas

--- homework-Gabija/3-darbas.js
+++ homework-Gabija/3-darbas.js
@@ -1,8 +1,50 @@
-//1. Susikurkite sąrašą 10 miestų, kuriame būtų įvardinami miesto pavadinimai ir jų atstumai iki Vilniaus ([‘Kaunas’, 89.6, ‘Klaipėda’, 307.3, … ).
-   // a. Parašykite funkciją, kuri pasakytų, kur toliausiai nuvažiuotume važiuodami x km/h greičiu per y valandų
-   // b. Parašykite funkciją, kuri pasakytų, ar spėtume pasiekti norimą miestą per x valandų ir kokiu greičiu turėtume važiuoti? (padarykite limitą 120 km/h)
-  // c. Parašykite funkciją, kuri nurodytų į kokį tolimiausią miestą mes galėtume nuvažiuoti su k kiekiu pinigų, jei už vieną litrą degalų turime mokėti j eurų. Kelionę pradedame su 30 litrų benzino bakelyje, už kuriuos mokėti nereikia. Nuvažiuodami vieną kilometrą sudeginame x kiekį degalų
-   // d. Susikurkite sąrašą kelio sąlygoms iki kiekvieno miesto apibūdinti (pavyzdžiui: geras, prastas, taisomas). Geromis kelio sąlygomis iki miesto važiuojame paprastai, prastomis kelio sąlygomis iki miesto važiuojame 1.5 karto ilgiau, o taisomo kelio sąlygomis važiuojame 2 kartus ilgiau. Parašykite funkciją, kuri pagal kelio sąlygas ir duotus atstumus, parašytų kiek laiko tektų važiuoti iki kiekvieno miesto.
+
+/*1. Susikurkite sąrašą 10 miestų, kuriame būtų įvardinami miesto pavadinimai ir jų atstumai iki Vilniaus ([‘Kaunas’, 89.6, ‘Klaipėda’, 307.3, … ).
+    a. Parašykite funkciją, kuri pasakytų, kur toliausiai nuvažiuotume važiuodami x km/h greičiu per y valandų */
+    
+    
+    let miestuSarasas = [
+      'Kaunas', 102.5,
+      'Klaipėda', 311.4,
+      'Šiauliai', 214.6,
+      'Panevėžys', 135.2,
+      'Alytus', 105.4,
+      'Marijampolė', 139.7,
+      'Utena', 96.8,
+      'Telšiai', 278.9,
+      'Tauragė', 225.6,
+      'Druskininkai', 129.3
+ ];
+
+ function kurNuvaziuotume(greitis, laikas) {
+  let rezultatas = '';
+  let nuvaziuotasAtstumas = greitis * laikas;
+  
+  for (let i = 0; i < miestuSarasas.length; i += 2) {
+    if (nuvaziuotasAtstumas >= miestuSarasas[i + 1]) {
+          if (rezultatas === '') {
+            rezultatas = `${miestuSarasas[i]} (${miestuSarasas[i + 1]} km)`;
+          } else {
+            rezultatas += `, ${miestuSarasas[i]} (${miestuSarasas[i + 1]} km)`;
+          }
+        }
+  }
+  
+  if (rezultatas === '') {
+    return `Per ${laikas} valandas negalėtume nuvažiuoti į jokį miestą.`;
+  }
+  
+  return `Per ${laikas} valandas nuvažiuotume į šiuos miestus: ${rezultatas}.`;
+}
+
+console.log(kurNuvaziuotume(90, 2));
+
+//b. Parašykite funkciją, kuri pasakytų, ar spėtume pasiekti norimą miestą per x valandų ir kokiu greičiu turėtume važiuoti? (padarykite limitą 120 km/h)
+
+//c. Parašykite funkciją, kuri nurodytų į kokį tolimiausią miestą mes galėtume nuvažiuoti su k kiekiu pinigų, jei už vieną litrą degalų turime mokėti j eurų. Kelionę pradedame su 30 litrų benzino bakelyje, už kuriuos mokėti nereikia. Nuvažiuodami vieną kilometrą sudeginame x kiekį degalų
+
+//d. Susikurkite sąrašą kelio sąlygoms iki kiekvieno miesto apibūdinti (pavyzdžiui: geras, prastas, taisomas). Geromis kelio sąlygomis iki miesto važiuojame paprastai, prastomis kelio sąlygomis iki miesto važiuojame 1.5 karto ilgiau, o taisomo kelio sąlygomis važiuojame 2 kartus ilgiau. Parašykite funkciją, kuri pagal kelio sąlygas ir duotus atstumus, parašytų kiek laiko tektų važiuoti iki kiekvieno miesto. */
+
 
 
 
@@ -10,10 +52,16 @@
 
 
 
-// 3. Susikurkite 7 sąrašus, kuriose yra talpinamos skirtingų dienų temperatūros vertės
-   // a. Parašykite funkciją, kuri apskaičiuotų norimos dienos vidutinę temperatūrą.
-   //  b. Parašykite funkciją, kuri apskaičiuotų norimos dienos aukščiausią temperatūrą.
-   // c. Parašykite funkciją, kuri apskaičiuotų norimos dienos žemiausią temperatūrą.
-   //  d. Parašykite funkciją, kuriai davus visas savaitės dienas, ji suranda kurią dieną buvo žemiausia temperatūra, ir kurią dieną buvo aukščiausia temperatūra
-   // e. Parašykite funkciją, kuri apskaičiuotų visos savaitės vidutinę temperatūrą
-   // f. Parašykite funkciją, kuriai nurodžius vieną savaitės dieną ir tos dienos numatomą kritulių kiekį (saulėta (nelis) / debesuota (lis)), funkcija praneštų kaip siūloma pasiruošti einant į lauką: Pasitepti saulės kremu (jei saulėta ir šilta), Pasiimti skėtį (jei debesuota ir nešalta), Užsidėti striukę (Jei šalta), ir pasiimti morką besmegeniui lipdyti (Jei šalta ir debesuota). Kokia temperatūra yra šilta/nešalta/šalta įsivardinkite
+
+
+
+
+
+
+/* 3. Susikurkite 7 sąrašus, kuriose yra talpinamos skirtingų dienų temperatūros vertės
+    a. Parašykite funkciją, kuri apskaičiuotų norimos dienos vidutinę temperatūrą.
+    b. Parašykite funkciją, kuri apskaičiuotų norimos dienos aukščiausią temperatūrą.
+    c. Parašykite funkciją, kuri apskaičiuotų norimos dienos žemiausią temperatūrą.
+    d. Parašykite funkciją, kuriai davus visas savaitės dienas, ji suranda kurią dieną buvo žemiausia temperatūra, ir kurią dieną buvo aukščiausia temperatūra
+    e. Parašykite funkciją, kuri apskaičiuotų visos savaitės vidutinę temperatūrą
+    f. Parašykite funkciją, kuriai nurodžius vieną savaitės dieną ir tos dienos numatomą kritulių kiekį (saulėta (nelis) / debesuota (lis)), funkcija praneštų kaip siūloma pasiruošti einant į lauką: Pasitepti saulės kremu (jei saulėta ir šilta), Pasiimti skėtį (jei debesuota ir nešalta), Užsidėti striukę (Jei šalta), ir pasiimti morką besmegeniui lipdyti (Jei šalta ir debesuota). Kokia temperatūra yra šilta/nešalta/šalta įsivardinkite */
