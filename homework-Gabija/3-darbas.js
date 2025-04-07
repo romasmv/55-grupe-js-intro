116fbe5e4fee9f5c0dbb6c8e28d317804bae699e
Roman <rmorozovas@gmail.com>
2025-04-07 08:00:22 +0300

    update 3-darbas(1c)

--- homework-Gabija/3-darbas.js
+++ homework-Gabija/3-darbas.js
@@ -15,35 +15,98 @@
       'Tauragė', 225.6,
       'Druskininkai', 129.3
  ];
-
+ 
  function kurNuvaziuotume(greitis, laikas) {
-  let rezultatas = '';
-  let nuvaziuotasAtstumas = greitis * laikas;
-  
-  for (let i = 0; i < miestuSarasas.length; i += 2) {
+   let rezultatas = '';
+   let nuvaziuotasAtstumas = greitis * laikas;
+   
+   for (let i = 0; i < miestuSarasas.length; i += 2) {
     if (nuvaziuotasAtstumas >= miestuSarasas[i + 1]) {
-          if (rezultatas === '') {
-            rezultatas = `${miestuSarasas[i]} (${miestuSarasas[i + 1]} km)`;
-          } else {
-            rezultatas += `, ${miestuSarasas[i]} (${miestuSarasas[i + 1]} km)`;
+      if (rezultatas === '') {
+        rezultatas = `${miestuSarasas[i]} (${miestuSarasas[i + 1]} km)`;
+      } else {
+        rezultatas += `, ${miestuSarasas[i]} (${miestuSarasas[i + 1]} km)`;
           }
         }
+      }
+      
+      if (rezultatas === '') {
+        return `Per ${laikas} valandas negalėtume nuvažiuoti į jokį miestą.`;
+      }
+      
+      return `Per ${laikas} valandas nuvažiuotume į šiuos miestus: ${rezultatas}.`;
+    }
+    
+    console.log(kurNuvaziuotume(110, 3));
+    console.clear();
+    
+    
+    //b. Parašykite funkciją, kuri pasakytų, ar spėtume pasiekti norimą miestą per x valandų ir kokiu greičiu turėtume važiuoti? (padarykite limitą 120 km/h)
+    
+function arPasiektumMiesta(miestas, laikas) {
+  let greitis=0;
+  
+  for (let i = 0; i < miestuSarasas.length; i += 2) {
+    if (miestuSarasas[i] === miestas) {
+      greitis = miestuSarasas[i + 1] / laikas;
+      
+      if (greitis <= 120) {
+        return `Per ${laikas} valandas nuvažiuotume į: ${miestas} važiuodami ${greitis} km/h`;
+      } else {
+        return `Per ${laikas} valandas negalėtume nuvažiuoti į ${miestas}.`;
+      }
+    }
   }
   
-  if (rezultatas === '') {
-    return `Per ${laikas} valandas negalėtume nuvažiuoti į jokį miestą.`;
-  }
-  
-  return `Per ${laikas} valandas nuvažiuotume į šiuos miestus: ${rezultatas}.`;
+  return `Miestas "${miestas}" nerastas.`;
 }
 
-console.log(kurNuvaziuotume(90, 2));
+console.log(arPasiektumMiesta('Panevėžys',2));
+console.clear();
 
-//b. Parašykite funkciją, kuri pasakytų, ar spėtume pasiekti norimą miestą per x valandų ir kokiu greičiu turėtume važiuoti? (padarykite limitą 120 km/h)
 
 //c. Parašykite funkciją, kuri nurodytų į kokį tolimiausią miestą mes galėtume nuvažiuoti su k kiekiu pinigų, jei už vieną litrą degalų turime mokėti j eurų. Kelionę pradedame su 30 litrų benzino bakelyje, už kuriuos mokėti nereikia. Nuvažiuodami vieną kilometrą sudeginame x kiekį degalų
 
-//d. Susikurkite sąrašą kelio sąlygoms iki kiekvieno miesto apibūdinti (pavyzdžiui: geras, prastas, taisomas). Geromis kelio sąlygomis iki miesto važiuojame paprastai, prastomis kelio sąlygomis iki miesto važiuojame 1.5 karto ilgiau, o taisomo kelio sąlygomis važiuojame 2 kartus ilgiau. Parašykite funkciją, kuri pagal kelio sąlygas ir duotus atstumus, parašytų kiek laiko tektų važiuoti iki kiekvieno miesto. */
+/*   let miestuSarasas = [
+     'Kaunas', 102.5,
+     'Klaipėda', 311.4,
+    'Šiauliai', 214.6,
+    'Panevėžys', 135.2,
+    'Alytus', 105.4,
+    'Marijampolė', 139.7,
+    'Utena', 96.8,
+    'Telšiai', 278.9,
+    'Tauragė', 225.6,
+    'Druskininkai', 129.3 ];   */
+
+function koksTolimiausiasMiestas(kuroKaina,pinigai) {
+  let pradedameKelione = 30;
+  let masinosSanaudos = 12;//litru kiekis / nuvaziuoto atstumo
+  let rezultatas = '';
+  for (let i = 0; i < miestuSarasas.length; i+=2) {
+    if (miestuSarasas[i+1]<=(((pinigai/kuroKaina)+pradedameKelione)/masinosSanaudos)*100) {
+      if (rezultatas === '') {
+        rezultatas = `${miestuSarasas[i]} (${miestuSarasas[i + 1]} km)`;
+      } else {
+        rezultatas += `, ${miestuSarasas[i]} (${miestuSarasas[i + 1]} km)`;
+          }
+        }
+      }
+      
+      if (rezultatas === '') {
+        return `Uz ${pinigai} euru negalėtume nuvažiuoti į jokį miestą.`;
+      }
+      
+      return `Uz ${pinigai} euru nuvažiuotume į šiuos miestus: ${rezultatas}.`;
+    }
+
+
+console.log(koksTolimiausiasMiestas(1.5,0));
+
+
+
+
+// d. Susikurkite sąrašą kelio sąlygoms iki kiekvieno miesto apibūdinti (pavyzdžiui: geras, prastas, taisomas). Geromis kelio sąlygomis iki miesto važiuojame paprastai, prastomis kelio sąlygomis iki miesto važiuojame 1.5 karto ilgiau, o taisomo kelio sąlygomis važiuojame 2 kartus ilgiau. Parašykite funkciją, kuri pagal kelio sąlygas ir duotus atstumus, parašytų kiek laiko tektų važiuoti iki kiekvieno miesto. 
 
 
 
